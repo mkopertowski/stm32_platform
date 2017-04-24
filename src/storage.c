4c55d282cd95f2a1b296d257f80a1074248de362
Miroslaw Kopertowski <miroslaw.kopertowski@tieto.com>
2017-04-24 13:01:32 +0200

    storage: function implementation added

--- src/storage.c
+++ src/storage.c
@@ -5,8 +5,11 @@
 typedef enum {
     STORAGE_ADDRESS_CELL1_CALIBRATION_FACTOR,
     STORAGE_ADDRESS_CELL2_CALIBRATION_FACTOR,
-    STORAGE_ADDRESS_CELL_ENABLED,
+    STORAGE_ADDRESS_CELL1_ENABLED,
+    STORAGE_ADDRESS_CELL2_ENABLED,
     STORAGE_ADDRESS_ATM_PRESSURE,
+    STORAGE_ADDRESS_DIVE_MODE,
+    STORAGE_ADDRESS_BATTERY_MONITOR_FACTOR,
 } storage_address_t;
 
 /* Virtual address defined by the user: 0xFFFF value is prohibited */
@@ -14,10 +17,10 @@ uint16_t VirtAddVarTab[] = {
         0x0000,              /* 2bytes, cell 1 calibration factor */
         0x0001,              /* 2bytes, cell 2 calibration factor */
         0x0002,              /* 2bytes, cell1, cell2 enabled */
-        0x0003,              /* 2bytes, atm pressure */
-        0x0004,
-        0x0005,
-        0x0006,
+        0x0003,              /* 2bytes, cell1, cell2 enabled */
+        0x0004,              /* 2bytes, atm pressure */
+        0x0005,              /* 2bytes, dive mode */
+        0x0006,              /* 2bytes, battery monitor factor */
         0x0007,
 };
 
@@ -58,28 +61,39 @@ void storage_set_atmospherePressure(uint16_t val)
     EE_WriteVariable(VirtAddVarTab[STORAGE_ADDRESS_ATM_PRESSURE], val);
 }
 
-
 bool storage_isSensorEnabled(uint8_t cellNr)
 {
-    return true;
+    uint16_t tmp_val;
+
+    EE_ReadVariable(VirtAddVarTab[STORAGE_ADDRESS_CELL1_ENABLED]+cellNr,(uint16_t*)&tmp_val);
+
+    return (bool)tmp_val;
 }
 
 storge_set_sensorEnable(uint8_t cellNr, bool enabled)
 {
-    //EE_WriteVariable(VirtAddVarTab[STORAGE_ADDRESS_ATM_PRESSURE], val);
+    EE_WriteVariable(VirtAddVarTab[STORAGE_ADDRESS_CELL1_ENABLED]+cellNr, enabled);
 }
 
 bool storage_is_diveMode(void)
 {
-    return false;
+    uint16_t tmp_val;
+
+    EE_ReadVariable(VirtAddVarTab[STORAGE_ADDRESS_DIVE_MODE],(uint16_t*)&tmp_val);
+
+    return (bool)tmp_val;
 }
 
 void storage_set_diveMode(bool val)
 {
-
+    EE_WriteVariable(VirtAddVarTab[STORAGE_ADDRESS_DIVE_MODE], val);
 }
 
 uint16_t storage_get_batteryMonitorFactor(void)
 {
-    return 123;
+    uint16_t tmp_val;
+
+    EE_ReadVariable(VirtAddVarTab[STORAGE_ADDRESS_BATTERY_MONITOR_FACTOR],(uint16_t*)&tmp_val);
+
+    return tmp_val;
 }
